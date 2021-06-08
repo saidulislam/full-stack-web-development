6a1667a581e41d340166132a5f8251bf87fdfce4
saidulislam <saidul.islam@gmail.com>
2021-06-08 12:43:41 -0400

    create function with ajax working

---
+++ crud-app-with-sqlalchemy/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, \
+    render_template, \
+        request,  \
+        redirect, \
+        url_for, \
+        jsonify
+        
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://saidulislam:T0pSecret@localhost:5432/simple_todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return (f'<Todo {self.id} {self.description}>')
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # description = request.form.get('description', '') # don't use this with ajax call
+    description = request.get_json()['description'] # use it for ajax call
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    #return redirect(url_for('index')) # redirects to the index route without ajax
+    return jsonify({
+        'description' : todo.description
+    })
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+if __name__ == '__main__':
+  app.run()
